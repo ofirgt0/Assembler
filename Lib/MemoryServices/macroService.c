a580e781c3d87d372192a3c5fc049b60163f5065
ofir ginat <ofirgt0@gmail.com>
2023-06-02 20:17:01 +0300

    ofir + liron macro/; set new line in macro

--- Lib/MemoryServices/macroService.c
+++ Lib/MemoryServices/macroService.c
@@ -5,11 +5,15 @@
 
 // Structure for a line in the macro
 struct Line
-{
+{ // TODO: MOVE THIS STRUCT TO DATA STRUCTURE
+    int commandIndex;
     char code;
     int opcode;
     int dstRegister;
     int srcRegister;
+    int address;
+    char *label;
+    char *originalCommand;
     struct Line *next;
 };
 
@@ -24,32 +28,53 @@ static struct Macro *macroHead = NULL;
 
 void addNewMacro(char *macroName, char *fileName, struct Line line)
 {
-    struct Macro *newMacro = (struct Macro *)malloc(sizeof(struct Macro));
-
+    struct Macro *newMacro = getMacro(macroName);
     if (newMacro == NULL)
+    {
+        newMacro = (struct Macro *)malloc(sizeof(struct Macro));
+
+        if (newMacro == NULL)
+        {
+            // Error: Memory allocation failed
+            printf("Error: Memory allocation failed\n");
+            return;
+        }
+        strcpy(newMacro->macroName, macroName);
+        newMacro->next = NULL;
+
+        struct Macro *currentMacro = macroHead;
+        while (currentMacro->next != NULL)
+            currentMacro = currentMacro->next;
+
+        currentMacro->next = newMacro;
+    }
+
+    struct Line *newLine = (struct Line *)malloc(sizeof(struct Line));
+
+    if (newLine == NULL)
     {
         // Error: Memory allocation failed
         printf("Error: Memory allocation failed\n");
         return;
     }
-    strcpy(newMacro->macroName, macroName);
-    newMacro->lines = line; // TODO: FIX THIS - ERROR WHILE POINTER = STRUCT
-    newMacro->next = NULL;
 
-    struct Macro *currentMacro = macroHead;
-    while (currentMacro->next != NULL)
-    {
-        currentMacro = currentMacro->next;
-    }
-    currentMacro->next = newMacro;
+    newLine->code = line.code;
+    newLine->opcode = line.opcode;
+    newLine->dstRegister = line.dstRegister;
+    newLine->srcRegister = line.srcRegister;
+    newLine->address = line.address;
+    newLine->label = line.label;
+    newLine->originalCommand = line.originalCommand;
+    newLine->commandIndex = line.commandIndex;
+    newLine->next = NULL;
 
-    if (isFileExist(fileName))
-    {
-        writeToFile(fileName, "New row to be added.");
-    }
+    struct Line *currentLine = macroHead->lines;
+    while (currentLine->next != NULL)
+        currentLine = currentLine->next;
+    currentLine->next = newLine;
 }
 
-getMacro(char *macroName)
+struct Macro *getMacro(char *macroName)
 {
     struct Macro *macro = macroHead;
     while (macro != NULL)
@@ -66,7 +91,7 @@ bool isMacroName(char *macroName)
     return getMacro(macroName) != NULL;
 }
 
-bool setExistMacro(char *macroName)
+bool setExistMacro(char *macroName, char *fileName, struct Line line)
 {
     // struct Macro* currentMacro = macroHead;
     // while (currentMacro != NULL){
@@ -89,4 +114,5 @@ void sendMacro(char *macroName)
     struct Line *currentLine = macro->lines;
     while (currentLine != NULL)
     {
+    }
 }
