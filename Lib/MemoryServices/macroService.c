d9f57c9e3af6e91f3c94660c24e547c45663a4c8
ofir ginat <ofirgt0@gmail.com>
2023-06-02 08:04:09 +0300

    LIRON EDITS - WITH ERRORS

--- Lib/MemoryServices/macroService.c
+++ Lib/MemoryServices/macroService.c
@@ -20,20 +20,60 @@ struct Macro {
 
 static struct Macro* macroHead = NULL;
 
-void addMacro(char* macroName, char* fileName, struct Line* linesHead){
-    
+void addNewMacro(char *macroName, char *fileName, struct Line line)
+{
+    struct Macro *newMacro = (struct Macro *)malloc(sizeof(struct Macro));
+
+    if (newMacro == NULL)
+    {
+        // Error: Memory allocation failed
+        printf("Error: Memory allocation failed\n");
+        return;
+    }
+    strcpy(newMacro->macroName, macroName);
+    newMacro->lines = line; //TODO: FIX THIS - ERROR WHILE POINTER = STRUCT
+    newMacro->next = NULL;
+
+    struct Macro *currentMacro = macroHead;
+    while (currentMacro->next != NULL)
+    {
+        currentMacro = currentMacro->next;
+    }
+    currentMacro->next = newMacro;
+
+    if (isFileExist(fileName))
+    {
+        writeToFile(fileName, "New row to be added.");
+    }
+}
+
+getMacro(char *macroName)
+{
+    struct Macro *macro = macroHead;
+    while (macro != NULL)
+    {
+        if (strcmp(macro->macroName, macroName) == 0)
+            return macro;
+        macro = macro->next;
+    }
+    return NULL;
+}
+
+bool isMacroName(char *macroName)
+{
+    return getMacro(macroName) != NULL;
 }
 
 bool setExistMacro(char* macroName){
-    struct Macro* currentMacro = macroHead;
-    while (currentMacro != NULL){
-        if (strcmp(currentMacro->macroName, macroName) == 0)
-            return true;
-        currentMacro = currentMacro->next;
-    }
-    return false;
+    // struct Macro* currentMacro = macroHead;
+    // while (currentMacro != NULL){
+    //     if (strcmp(currentMacro->macroName, macroName) == 0)
+    //         return true;
+    //     currentMacro = currentMacro->next;
+    // }
+    // return false;
 }
 
 void sendMacro(){
-    
+
 }
